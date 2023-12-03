f7473c87c2ce163e707877505218176f1ccc98bf
radixxx <radov.2011@gmail.com>
2023-12-03 23:55:00 +0200

    updated 0.1

--- app.go
+++ app.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	var name = "TOM"
 	fmt.Print(name)
+
+	var firstName, age = "Dann", 77
+	var x = fmt.Sprintln("\n My name is %s. and age %d: ", firstName, age)
+	fmt.Println(x)
 }
