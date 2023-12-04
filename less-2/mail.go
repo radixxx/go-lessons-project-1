9869f303d7713770bf9ac75809adc17b22e13acc
radixxx <radov.2011@gmail.com>
2023-12-05 00:36:22 +0200

    loop basics

--- less-2/mail.go
+++ less-2/mail.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+const pi float32 = 3.14
+const carMaxSpeed = 310
+
 func main() {
-	fmt.Print("Hello !")
+	fmt.Println(pi, carMaxSpeed)
+	fmt.Println(threeArguments())
+	fmt.Println(nonDeclaredReturn())
+	fmt.Println(loop())
+}
+
+func threeArguments() (string, string, string) {
+	a := "Hello"
+	b := "programmer"
+	c := "mundo"
+
+	return a, b, c
+}
+
+func nonDeclaredReturn() (f string) {
+	f = "Return Type !"
+	return
+}
+
+func loop() (sum int) {
+	sum = 0
+	count := 10
+	for i := 0; i < count; i++ {
+		sum += i
+	}
+	return sum
 }
