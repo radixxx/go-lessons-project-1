27241879c4c0b5b6d48beaf89f0444a0bcdf7d31
radixxx <radov.2011@gmail.com>
2023-12-05 23:50:29 +0200

    added statements

--- less-2/mail.go
+++ less-2/mail.go
@@ -10,6 +10,9 @@ func main() {
 	fmt.Println(threeArguments())
 	fmt.Println(nonDeclaredReturn())
 	fmt.Println(loop())
+	fmt.Println(anotherLoop())
+	statement()
+	swithStatement(2)
 }
 
 func threeArguments() (string, string, string) {
@@ -33,3 +36,47 @@ func loop() (sum int) {
 	}
 	return sum
 }
+
+func anotherLoop() (sum int) {
+	// like while loop
+	sum = 0
+	for sum < 100 {
+		sum += 5
+	}
+	return
+}
+
+func statement() {
+	a := 1
+	for a < 10 {
+		if a == 5 {
+			fmt.Println("*** a == 5!")
+		} else {
+			fmt.Printf("\na == %d", a)
+		}
+		a++
+	}
+}
+
+func isTrue(a int) int {
+	if a == 3 {
+		return 1
+	} else if a == 5 {
+		return 2
+	}
+	return 0
+}
+
+func swithStatement(x int) {
+	switch x {
+	case 1:
+		fmt.Println("\nShow: 1")
+	case 2:
+		fmt.Println("\nShow: 2")
+	default:
+		fmt.Print("\nby Default !")
+	}
+}
+
+// TODO: implement func with if statements
+func resolve() {}
