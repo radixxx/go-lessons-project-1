5d2e4fb860ac451fd1629670657ea824a00c3895
radixxx <radov.2011@gmail.com>
2023-12-11 23:04:52 +0200

    work with map

--- less-5/iterator.go
+++ less-5/iterator.go
@@ -4,7 +4,18 @@ import (
 	"fmt"
 )
 
+type Point struct {
+	x int
+	y int
+}
+
 func main() {
+	rangeType()
+	anMap()
+
+}
+
+func rangeType() {
 	storage := [3]int{64, 128, 256}
 	for index, element := range storage {
 		fmt.Printf("element at index %d is %d\n", index, element)
@@ -23,4 +34,20 @@ func main() {
 	for i := len(index) - 1; i >= 0; i-- {
 		fmt.Printf("Descending index - %d element: %d\n", i, index[i])
 	}
+}
+
+func anMap() {
+	pointMap := map[string]Point{}
+	anotherPointsMap := make(map[int]Point)
+
+	pointMap["a"] = Point{
+		x: 2,
+		y: 4,
+	}
+
+	fmt.Println(pointMap)
+	fmt.Println(pointMap["a"])
+	anotherPointsMap[1] = Point{3, 6}
+	fmt.Println(anotherPointsMap)
+	fmt.Println(anotherPointsMap[1])
 }
