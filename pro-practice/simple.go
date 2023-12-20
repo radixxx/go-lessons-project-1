602ea80273f2fcc423ba05d4472914f4fb1b6bae
radixxx <radov.2011@gmail.com>
2023-12-20 23:25:01 +0200

    added waitGroup

--- pro-practice/simple.go
+++ pro-practice/simple.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"time"
+	"sync"
 )
 
 func main() {
@@ -13,12 +13,18 @@ func main() {
 	count := *n
 	fmt.Printf("Going to create %d goroutines.\n", count)
 
+	var waitGroup sync.WaitGroup
+
+	fmt.Printf("%#v\n", waitGroup)
 	for i := 0; i < count; i++ {
+		waitGroup.Add(1)
 		go func(x int) {
+			defer waitGroup.Done()
 			fmt.Printf("%d ", x)
 		}(i)
 	}
 
-	time.Sleep(time.Second)
+	fmt.Printf("%#v\n", waitGroup)
+	waitGroup.Wait()
 	fmt.Println("\nExiting...")
 }
