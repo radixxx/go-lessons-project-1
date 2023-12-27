bf3a05cb44f03a55ee9272fb0071e6438d32c265
radixxx <radov.2011@gmail.com>
2023-12-27 21:12:58 +0200

    con-2

--- pro-practice/con.go
+++ pro-practice/con.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
+	"time"
 )
 
 var CLOSE = false
 var DATA = make(map[int]bool)
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Need two integer parameters !")
+		return
+	}
+
+	n1, _ := strconv.Atoi(os.Args[1])
+	n2, _ := strconv.Atoi(os.Args[2])
+
+	if n1 > n2 {
+		fmt.Printf("%d should be smaller than %d\n", n1, n2)
+		return
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	A := make(chan int)
+	B := make(chan int)
+
+	go first(n1, n2, A)
+	go second(B, A)
+	third(B)
 }
 
 func first(min, max int, out chan<- int) {
@@ -22,4 +46,28 @@ func first(min, max int, out chan<- int) {
 
 func random(min, max int) int {
 	return rand.Intn(max-min) + min
+}
+
+func second(out chan<- int, in <-chan int) {
+	for x := range in {
+		fmt.Print(x, " ")
+		_, ok := DATA[x]
+		if ok {
+			CLOSE = true
+		} else {
+			DATA[x] = true
+			out <- x
+		}
+	}
+	fmt.Println()
+	close(out)
+}
+
+func third(in <-chan int) {
+	var sum int
+	sum = 0
+	for x2 := range in {
+		sum = sum + x2
+	}
+	fmt.Println("The sum of random numbers in %d\n", sum)
 }
