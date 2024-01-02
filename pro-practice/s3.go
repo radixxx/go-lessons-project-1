a6ed7c0bff9a6b0c1a8ad67b2ad4664ea3b258b6
radixxx <radov.2011@gmail.com>
2024-01-02 15:27:35 +0200

    added kitchen format

--- pro-practice/s3.go
+++ pro-practice/s3.go
@@ -31,9 +31,16 @@ func myHandler(w http.ResponseWriter, r *http.Request) {
 
 func timeHandler(w http.ResponseWriter, r *http.Request) {
 	t := time.Now().Format(time.RFC1123)
+	g := time.Now().Local().Format(time.Kitchen)
 	Body := "The current time is:"
 	fmt.Fprintf(w, "<h1 align=\"center\">%s</h1>", Body)
 	fmt.Fprintf(w, "<h2 align=\"center\">%s</h2>\n", t)
 	fmt.Fprintf(w, "Serving: %s\n", r.URL.Path)
 	fmt.Printf("Served time for: %s\n", r.Host)
+
+	KitchenTime := "Kitcehn time is: "
+	fmt.Fprintf(w, "<h1 align=\"center\">%s</h1>", KitchenTime)
+	fmt.Fprintf(w, "<h2 alieg=\"center\">%s</h2>", g)
+	fmt.Fprintf(w, "Serving time: %s\n", r.URL.Path)
+	fmt.Printf("Serving kitchen time: %s\n", r.Host)
 }
